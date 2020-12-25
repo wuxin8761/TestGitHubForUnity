974613dbc02508682288fd421bd482889e3d1ec1
wuxin8761 <wuxin8761@126.com>
2020-12-25 12:55:04 +0800

    修改test.cs脚本

--- TestGitHubForUnity2019/Assets/Scripts/test.cs
+++ TestGitHubForUnity2019/Assets/Scripts/test.cs
@@ -1,18 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class test : MonoBehaviour
 {
+    /// <summary>
+    /// 测试
+    /// </summary>
+    public GameObject _Player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // TODO: 测试使用
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("OK!");
+        }
     }
 }
